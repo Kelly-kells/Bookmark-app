97bad5e97772d3d7115ed15bae858e11aeb2dc18
kelly-kells <kellskelechi@gmail.com>
2021-08-18 21:56:01 +0100

    setting the local storage

--- js/main.js
+++ js/main.js
@@ -16,9 +16,19 @@ function saveBookmark(e) {
         var bookmarks = [];
 
         bookmarks.push('bookmark');
-        localStorage.getItem('bookmarks', JSON.stringify(bookmarks));
+        localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
         
+    } else{
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        bookmarks.push(bookmark)
+        localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
     }
     
     e.preventDefault();
 }
+    function fetchBookmarks() {
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+        console.log(bookmarks);
+        
+    }
